3fdc37bae2f71c0cf9545f652d0cb8b0ae1762f0
Eric Sherman <ericandrewsherman@gmail.com>
2017-04-20 13:27:47 -0500

    change intro text

--- js/userconfig.js
+++ js/userconfig.js
@@ -15,5 +15,5 @@ var config = {
     //Change to your username, insert function on cartodb, and cartodb tablename (see also /cartoDB_functions)
 	cartoDBusername : 'easherma',
 	cartoDBinsertfunction : 'insert_bikeways_data',
-	walkthroughWelcome: "<p>This webmap allows you to view data collected by the <a href='http://bostoncyclistsunion.org/' target='_blank'>Boston Cyclists Union</a> on biking in Boston.</p><p>It also allows you to submit input on where biking infrastructure can be improved by drawing on the map! The map is restricted to the inner core of Metro Boston, where we concentrate our advocacy.</p>"
+	walkthroughWelcome: "<p>This webmap allows you to submit input on where critical rural freight coordiors can be improved by drawing on the map. It also allows you to view relevant data we have collected.</p>"
 };

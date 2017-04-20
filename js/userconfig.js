1f67b74ae3db662a5923cacd073987c25aaa1ef1
Eric Sherman <ericandrewsherman@gmail.com>
2017-04-20 12:09:28 -0500

    update config

--- js/userconfig.js
+++ js/userconfig.js
@@ -10,10 +10,10 @@ var config = {
 	north : 42.453,
 	east : -70.95,
 	//Mapbox access token & key for basemap
-	mapboxAccessToken :              
-            'pk.eyJ1IjoicmVtb3RlZ2VudHJpZnkiLCJhIjoiY2lnanJzMjJpMDA1dnYxbHo5MTZtdGZsYSJ9.gLE8d40zmDAtMSSZyd2h1Q',
+	mapboxAccessToken :
+            'pk.eyJ1IjoiZWFzaGVybWEiLCJhIjoiY2oxcW51Nzk2MDBkbTJxcGUxdm85bW5xayJ9.7mL0wQ7cjifWwt5DrXMuJA',
     //Change to your username, insert function on cartodb, and cartodb tablename (see also /cartoDB_functions)
-	cartoDBusername : 'raphaeld',
+	cartoDBusername : 'easherma',
 	cartoDBinsertfunction : 'insert_bikeways_data',
 	walkthroughWelcome: "<p>This webmap allows you to view data collected by the <a href='http://bostoncyclistsunion.org/' target='_blank'>Boston Cyclists Union</a> on biking in Boston.</p><p>It also allows you to submit input on where biking infrastructure can be improved by drawing on the map! The map is restricted to the inner core of Metro Boston, where we concentrate our advocacy.</p>"
 };
